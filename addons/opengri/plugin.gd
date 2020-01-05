358c2aac3057c5474887cc44991d67e548a53d62
q4a <q4arus@ya.ru>
2020-01-05 23:45:17 +0300

    add basic doc with one button

---
+++ addons/opengri/plugin.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorPlugin
+
+var dock
+
+func _enter_tree():
+	dock = preload("res://addons/opengri/dock.tscn").instance()
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
+
+func _exit_tree():
+	remove_control_from_docks(dock)
+	dock.free()
