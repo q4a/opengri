51279fb636d9176516b0d7ee0cc0cec327f74884
q4a <q4arus@ya.ru>
2020-01-07 05:22:28 +0300

    add main parts of file manager

--- addons/opengri/scripts/OpenGRI.gd
+++ addons/opengri/scripts/OpenGRI.gd
@@ -1,12 +1,30 @@
 tool
 extends EditorPlugin
 
-var dock
+var doc = preload("../scenes/FileEditor.tscn").instance()
+
+var IconLoader = preload("IconLoader.gd").new()
 
 func _enter_tree():
-	dock = preload("res://addons/opengri/scenes/MainDock.tscn").instance()
-	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
+	add_autoload_singleton("IconLoader","res://addons/opengri/scripts/IconLoader.gd")
+	add_autoload_singleton("LastOpenedFiles","res://addons/opengri/scripts/LastOpenedFiles.gd")
+	get_editor_interface().get_editor_viewport().add_child(doc)
+	doc.hide()
 
 func _exit_tree():
-	remove_control_from_docks(dock)
-	dock.free()
+	doc.clean_editor()
+	get_editor_interface().get_editor_viewport().remove_child(doc)
+	remove_autoload_singleton("IconLoader")
+	remove_autoload_singleton("LastOpenedFiles")
+
+func has_main_screen():
+	return true
+
+func get_plugin_name():
+	return "ResImporter"
+
+func get_plugin_icon():
+	return IconLoader.load_icon_from_name("file")
+
+func make_visible(visible):
+	doc.visible = visible
