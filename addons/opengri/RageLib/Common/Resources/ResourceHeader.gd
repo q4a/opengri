898605a038b8ec540ac291cf64d21044bc1aa1ff
q4a <q4arus@ya.ru>
2020-01-19 09:32:49 +0300

    Finish with RageLib/Common/Resources/ResourceHeader.gd except BinaryReader and BinaryWriter

--- addons/opengri/RageLib/Common/Resources/ResourceHeader.gd
+++ addons/opengri/RageLib/Common/Resources/ResourceHeader.gd
@@ -20,8 +20,6 @@
 
 extends Node
 
-#const ResourceType = preload("res://addons/OpenGRI/RageLib/Common/Resources/ResourceType.gd").ResourceType
-
 class ResourceHeader:
 	const MagicBigEndian: int = 0x52534305
 	const MagicValue: int = 0x05435352
@@ -29,3 +27,46 @@ class ResourceHeader:
 	var Magic: int
 	var Type: int
 	var Flags: int
+	var CompressCodec: int
+	
+	func GetSystemMemSize() -> int:
+		return int(Flags & 0x7FF) << int(((Flags >> 11) & 0xF) + 8)
+	
+	func GetGraphicsMemSize() -> int:
+		return int((Flags >> 15) & 0x7FF) << int(((Flags >> 26) & 0xF) + 8)
+	
+	func SetMemSizes(systemMemSize: int, graphicsMemSize: int) -> void:
+		
+		# gfx = a << (b + 8)
+		# minimum representable is block of 0x100 bytes
+		
+		var maxA: int = 0x3F
+		
+		var sysA: int = systemMemSize >> 8;
+		var sysB: int = 0
+		
+		while(sysA > maxA):
+			if ((sysA & 1) != 0):
+				sysA += 2
+			sysA >>= 1
+			sysB += 1
+		
+		var gfxA: int = graphicsMemSize >> 8
+		var gfxB: int = 0
+		
+		while (gfxA > maxA):
+			if ((gfxA & 1) != 0):
+				gfxA += 2
+			gfxA >>= 1
+			gfxB += 1
+			
+			Flags = (Flags & 0xC0000000) | int(sysA | (sysB << 11) | (gfxA << 15) | (gfxB << 26))
+	
+	func Read(br) -> void:
+		print("Implement BinaryReader!")
+		print_stack()
+	
+	func Write(bw) -> void:
+		print("Implement BinaryWriter!")
+		print_stack()
+		
